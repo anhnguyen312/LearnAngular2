2db6af0babf6355d0e995dd3522be25d0ee07c84
anh nguyen <anh.nguyen@tpssoft.com>
2017-10-11 09:47:15 +0700

    cm3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,50 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroComponent } from './hero.component';
+import { HeroService } from './hero.service';
+
+import { RouterModule }   from '@angular/router';
+
+import { DashboardComponent }   from './dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroDetailComponent,
+    HeroComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroComponent
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      },
+      {
+        path: 'detail/:id',
+        component: HeroDetailComponent
+      }
+    ])
   ],
-  providers: [],
+  providers: [
+    HeroService
+  ],
   bootstrap: [AppComponent]
 })
+
+
 export class AppModule { }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import{MyHero} from './MyHero';
+export const HEROES : MyHero[] = [
+    {id : 1, name : 'Storm Spirit'},
+    {id : 2, name : 'Invoker'},
+    {id : 3, name : 'Blood Seeker'},
+    {id : 4, name : 'Earth Shaker'},
+    {id : 5, name : 'Lina'},
+    {id : 6, name : 'Dragon Knight'},
+    {id : 7, name : 'Slandar'},
+    {id : 8, name : 'Centaur Warior'},
+    {id : 9, name : 'Troll'},
+    {id : 10, name : 'Phantom Assassin'},
+    {id : 11, name : 'Faceless Void'}
+];
